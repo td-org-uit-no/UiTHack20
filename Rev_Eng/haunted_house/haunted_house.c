c5a9eaeb039171ac6cf9f8ec1fc50982f77dda3f
Steinar30 <steinarhansen352@gmail.com>
2020-08-26 17:16:51 +0200

    :derelict_house: first draft of haunted house reveng

---
+++ Rev_Eng/haunted_house/haunted_house.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void enter_pass(char* qnumber, char* correct)
+{
+    char guess[31];
+    while (1) {
+        printf("Enter %s passphrase: \n", qnumber);
+        scanf("%30s", guess);
+        if (strcmp(correct, guess) == 0) { // equal = 0
+            printf("%s is correct!\n", correct);
+            break;
+        } else {
+            printf("not correct \n");
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // TODO, maybe use a better flag
+    char* pass1 = "spooky";
+    char* pass2 = "scary";
+    char* pass3 = "skeleton";
+
+    enter_pass("first", pass1);
+    enter_pass("second", pass2);
+    enter_pass("third", pass3);
+
+    printf("The flag is UiTHack20{%s_%s_%s}", pass1, pass2, pass3);
+}
